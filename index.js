53d06f599572ac68db0649dae19e5669ace1fa6c
fypgeneral <fypgeneral@gmail.com>
2022-01-31 16:26:47 +0530

    2 files modified, 2 added

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-// run `node index.js` in the terminal
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoute = require('./user.js');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 8080;
+//Creating general middleware for all routes to overide cors and help send response to any server
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); //second param is name of domain
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // console.log('got request ')
+  next();
+});
+
+app.use('/users', userRoute);
+
+var listener = app.listen(PORT, () =>
+  console.log(`Server Running on Port: ${PORT}`)
+);
